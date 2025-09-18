3aca94761c252c5880e5a57ef28ac158f9393704
nguyentrungnam01n <nguyentrungnam01n@gmail.com>
2025-09-18 11:19:47 +0700

    fix size of video

--- src/sections/Overview.jsx
+++ src/sections/Overview.jsx
@@ -72,19 +72,18 @@ const Overview = () => {
           </div>
 
           {/* YouTube Video iframe */}
-          <div className="mb-12 text-center">
+          <div className="mb-16 text-center relative" style={{ paddingBottom: '56.25%' }}>
             <iframe
-              width="854"
-              height="480"
               src="https://www.youtube.com/embed/-hr-F2GxvLs"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
-              className="mx-auto rounded-xl"
+              className="absolute top-0 left-0 w-full h-full rounded-xl center"
             ></iframe>
           </div>
-          
+
+
           {/* Image - Full Width */}
           <div
             ref={imageRef}
